c5172f247b09a64fceba7e01de62d84f532ee127
Sithum Buddhika Jayalal <Officialsithumbuddhika@gmail.com>
2026-01-11 20:46:13 +0530

    Video-card.tsx and Video type fix

---
+++ types/index.ts
@@ -0,0 +1,10 @@
+export type Video = {
+  id: string;
+  title: string;
+  description?: string | null;
+  publicId: string;
+  originalSize: string;
+  compressedSize: string;
+  duration: number; // UI expects number
+  createdAt: string | Date;
+};
